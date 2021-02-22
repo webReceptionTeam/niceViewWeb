f53d412752c5555f0364f72428e0a11e3b28cf0c
jiangtao <liujiangtao@sinosoft.com.cn>
2021-02-22 21:13:01 +0800

    modified: 管理员头部配置

--- src/layout/administration/adminHeader/index.vue
+++ src/layout/administration/adminHeader/index.vue
@@ -1,17 +1,30 @@
 <template>
   <el-breadcrumb separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">{{
+      item.name
+    }}</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script>
+import { filterGetRoutePath } from "@/utils/filterData";
+import { ref } from "vue";
 export default {
   name: "adminHeader",
   setup() {
-    return {};
+    let breadcrumbList = ref([
+      {
+        name: "首页",
+        path: "",
+      },
+      {
+        name: "首页配置",
+        path: "viewConfigHome",
+      },
+    ]);
+    return {
+      breadcrumbList,
+    };
   },
 };
 </script>
