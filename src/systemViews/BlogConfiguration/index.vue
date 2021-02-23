03e4f62fac354c2789d486e65e9007144be0bb6a
jiangtao <liujiangtao@sinosoft.com.cn>
2021-02-23 23:11:32 +0800

    modfied: 添加配置页面

---
+++ src/systemViews/BlogConfiguration/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- 这里要做博客相关配置 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogConfiguration",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
