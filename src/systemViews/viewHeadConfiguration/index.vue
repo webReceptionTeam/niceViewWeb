16687e3648b6b38cf5e2eed0cdc81981b1f94f2e
71401851 <liujiangtao@sinosoft.com.cn>
2022-10-08 11:36:34 +0800

    modified: 修复文件名异常导致的启动报错

---
+++ src/systemViews/viewHeadConfiguration/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>头部配置</div>
+  </template>
+  
+  <script>
+  export default {
+    name: "viewHeadConfiguration",
+    setup() {
+      return {};
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
